8f82f05c1ca92231d98d8659c8cd1532a85a6a26
Ryan Jansen <rjgiga444@gmail.com>
2020-10-02 22:24:43 +0800

    commit all changes

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ app.use(bodyParser.json());
 const workouts = require("./api/workouts");
 app.use("/api/workouts", workouts);
 
-app.use(express.static(path.join(__dirname, "../build/index.html")));
+app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
