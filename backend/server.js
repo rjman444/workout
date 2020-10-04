7aa1b373817ff34f9c030a7f679155f6e8f179d4
Ryan Jansen <rjgiga444@gmail.com>
2020-10-04 14:27:29 +0800

    Added compression and helmet for express

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const compression = require("compression");
+const helmet = require("helmet");
 const cors = require("cors");
 const path = require("path");
+
 const app = express();
+
+app.use(compression());
+
+app.use(helmet());
+
 app.use(cors());
+
 app.options("*", cors());
+
 require("./database");
 
 app.use(bodyParser.json());
